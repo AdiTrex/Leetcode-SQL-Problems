17366862a278c2d190d2cb36e113536183ca62ee
AdiTrex <90887797+AdiTrex@users.noreply.github.com>
2023-09-06 17:01:30 +0530

    Time: 2103 ms (5.00%), Space: 0B (100.00%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,7 @@
+with cte as
+(
+select id,num,lead(num) over(order by id) as nxt,lag(num) over(order by id) as prev
+from Logs
+)
+
+select distinct num as ConsecutiveNums from cte where num = nxt and num = prev
